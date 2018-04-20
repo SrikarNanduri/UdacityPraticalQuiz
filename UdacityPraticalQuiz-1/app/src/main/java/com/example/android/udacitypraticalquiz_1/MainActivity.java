259d0811ffacba53ded9a1c174db0d12b3be9990
srikarn <srikarnanduri628@gmail.com>
2018-04-20 13:42:06 +0530

    Added images and set the main activity

--- UdacityPraticalQuiz-1/app/src/main/java/com/example/android/udacitypraticalquiz_1/MainActivity.java
+++ UdacityPraticalQuiz-1/app/src/main/java/com/example/android/udacitypraticalquiz_1/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.android.udacitypraticalquiz_1;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.transition.Transition;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    EditText user, email, descriptin ;
 
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(R.color.colorPrimaryDark);
+
+        user = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email);
+        descriptin =(EditText) findViewById(R.id.description);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_account) {
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
